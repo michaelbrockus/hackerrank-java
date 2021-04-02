cf891ec93ff7174cb88f9c5402cab73296054877
Michael <michaelbrockus@gmail.com>
2021-04-01 18:16:07 -0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "blog.home.michaelbrockus"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // testCompile("junit", "junit", "4.12")
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+//
+// file: Main.java
+// author: Michael Brockus
+// gmail: <michaelbrockus@gmail.com>
+// github: https://github.com/michaelbrockus
+//
+
+
+public class Main {
+
+    //
+    // function main begins all program execution
+    //
+    static public void main(String[] args)
+    {
+        System.out.println("We are ready for a cup of HackerRank");
+    } // end of method
+
+} // end of class

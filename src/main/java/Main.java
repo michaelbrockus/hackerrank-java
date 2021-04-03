b08866da10b8d258f71a17d08a8f73f54fc8dab4
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-03 05:46:25 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,34 @@
 // gmail: <michaelbrockus@gmail.com>
 // github: https://github.com/michaelbrockus
 //
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     //
     // function main begins all program execution
     //
-    static public void main(String[] args)
-    {
-        System.out.println("We are ready for a cup of HackerRank");
-    } // end of method main
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        if (N % 2 == 0) {
+            System.out.println("Not Weird");
+        } // end if
+        else {
+            System.out.println("Weird");
+        } // end else
+
+        scanner.close();
+    } // end of method main
+    
 } // end of class

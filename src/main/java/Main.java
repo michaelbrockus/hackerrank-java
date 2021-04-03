5372109a338b1bc8a06d131a77d9c23fef4dee41
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-03 06:08:47 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,18 @@ public class Main {
     //
     static public void main(String[] args)
     {
-        System.out.println("We are ready for a cup of HackerRank");
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); // remove extra newline before string for this case
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
     } // end of method main
 
 } // end of class

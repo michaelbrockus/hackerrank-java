07ee8da5c6fe87437dcbb966de77f79f60b2ad93
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-03 04:37:54 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,6 @@ public class Main {
     {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
-    } // end of method
+    } // end of method main
 
 } // end of class

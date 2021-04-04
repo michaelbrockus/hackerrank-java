ba0526065b1d36bd7dd469569cb86e0a0b8edf62
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-04 05:09:30 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,37 @@
 // gmail: <michaelbrockus@gmail.com>
 // github: https://github.com/michaelbrockus
 //
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    static int B = input.nextInt();
+    static int H = input.nextInt();
+    static Boolean flag = B > 0 && H > 0;
+
+    static {
+
+        if (!flag) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } // end if
+
+    } // end static block
 
     //
     // function main begins all program execution
     //
     static public void main(String[] args)
     {
-        System.out.println("We are ready for a cup of HackerRank");
+        if (flag) {
+			int area = B * H;
+			System.out.print(area);
+		} // end if
+
     } // end of method main
 
 } // end of class

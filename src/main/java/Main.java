877a737a9e106346eb70478bf50dcd733734a5b7
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-04 04:31:40 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,33 @@
 // gmail: <michaelbrockus@gmail.com>
 // github: https://github.com/michaelbrockus
 //
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final int MAX_ITER = 11;
 
     //
     // function main begins all program execution
     //
     static public void main(String[] args)
     {
-        System.out.println("We are ready for a cup of HackerRank");
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int iter = 1; iter < MAX_ITER; ++iter) {
+            System.out.printf("%d x %d = %d %n", N, iter, N * iter);
+        } // end for
+
+        scanner.close();
     } // end of method main
 
 } // end of class

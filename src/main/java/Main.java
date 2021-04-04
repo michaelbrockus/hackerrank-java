2f6f670f949eea19fe42f101faccd89e16f36fe0
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-04 04:48:05 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,45 @@ public class Main {
     //
     static public void main(String[] args)
     {
-        System.out.println("We are ready for a cup of HackerRank");
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; ++i)
+        {
+            try
+            {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                try {
+                    byte y = (byte)x;
+                    if(y == x) {
+                        System.out.println("* byte");    
+                    } // end if
+                } catch(Exception e){} // end try
+                try {
+                    short y = (short)x;
+                    if (y == x) {
+                        System.out.println("* short");
+                    } // end if
+                } catch(Exception e) {} // end try
+                try {
+                    int y = (int)x;
+                    if(y == x) {
+                        System.out.println("* int");
+                    } // end if
+                } catch(Exception e){} // end try
+                
+                System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            } // end try
+
+        } // end for
+
+        sc.close();
+
     } // end of method main
 
 } // end of class

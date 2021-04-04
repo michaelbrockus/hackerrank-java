713009bd90d7906f38c591ec7491fb98cdaaa00f
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-04-04 04:36:03 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,23 @@ public class Main {
     //
     static public void main(String[] args)
     {
-        System.out.println("We are ready for a cup of HackerRank");
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+
+            for(int j = 0; j < n; ++j)
+            {
+                calc += (Math.pow(2, j) * b);
+                System.out.print(calc + " ");
+            } // end for
+            System.out.println();
+        } // end for
+
+        in.close();
     } // end of method main
 
 } // end of class
